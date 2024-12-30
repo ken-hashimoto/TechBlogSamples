45cf6ee66dde6a9354de2fc2ad4020727d115314
Kenji Hashimoto <hashimoto.kenji.1102@gmail.com>
2024-12-30 18:01:59 +0900

    feat: 🎸 boolだけを返す関数の関数名がIsで始まっていない場合に警告をだすlinterを作成

---
+++ go-ruleguard-sample/go.mod
@@ -0,0 +1,5 @@
+module go-ruleguard-sample
+
+go 1.23.1
+
+require github.com/quasilyte/go-ruleguard/dsl v0.3.22
---
+++ go-ruleguard-sample/main.go
@@ -0,0 +1,7 @@
+package main
+
+func IsNegative(x int) bool {
+	return x < 0
+}
+
+func Zero(x int) bool { return x == 0 }
