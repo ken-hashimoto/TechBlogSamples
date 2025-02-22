2225f7e7286567ad10b523352796cf94c25cea32
Kenji Hashimoto <hashimoto.kenji.1102@gmail.com>
2025-02-22 20:08:30 +0900

    refactor: 💡 rename file name (types.go -> sample.go)

--- go-mvpkg/main.go
+++ go-mvpkg/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 
-	"github.com/TechBlogSamples/go-mvpkg/oldpackage"
+	"github.com/TechBlogSamples/go-mvpkg/newpackage"
 )
 
 func main() {
 	fmt.Println(oldpackage.Ken)
 	fmt.Println(oldpackage.MyFavoriteWord)
 
+	newpackage := "1" // 変更先のpackage名と同一の名前
+
 	Bob := oldpackage.Human{
 		Name: "Bob",
 		Age:  20,
 	}
 	fmt.Println(Bob)
+	fmt.Println(newpackage)
 }
